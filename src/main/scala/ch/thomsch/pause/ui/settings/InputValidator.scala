6cf5e16f1c2099b0e59c59b3f13e865f63dc2b85
Thomsch <thms.sch@gmail.com>
2017-09-11 15:20:51 +0200

    Improved input error's handling
    - Errors' highlight is more consistent with other programs
    - Simplified error highlighting (removed ExecutorService)
    - Extracted input verification to its own class

---
+++ src/main/scala/ch/thomsch/pause/ui/settings/InputValidator.scala
@@ -0,0 +1,37 @@
+package ch.thomsch.pause.ui.settings
+
+import javafx.beans.value.{ChangeListener, ObservableValue}
+import javafx.css.PseudoClass
+
+import scalafx.scene.control.{TextField, ToggleButton}
+
+/**
+  * Verifies the user inputs and highlight them if they are not correct.
+  * If any user input is not correct, it will disable the main button.
+  *
+  * @author Thomsch
+  */
+class InputValidator(private val workDuration: TextField,
+                     private val onOffButton: ToggleButton) extends ChangeListener[String] {
+  val STYLE_ERROR: PseudoClass = PseudoClass.getPseudoClass("error")
+
+  workDuration.text.addListener(this)
+
+  override def changed(observable: ObservableValue[_ <: String], oldValue: String, newValue: String): Unit = {
+    val workDurationIsInvalid = !isWorkDurationValid
+    workDuration.pseudoClassStateChanged(STYLE_ERROR, workDurationIsInvalid)
+    onOffButton.setDisable(workDurationIsInvalid)
+  }
+
+  /**
+    * Returns whether the work duration is valide.
+    * A work duration is valid if it's a integer number greater than zero.
+    */
+  def isWorkDurationValid: Boolean = {
+    try {
+      workDuration.getText.toLong >= 0
+    } catch {
+      case _: NumberFormatException => false
+    }
+  }
+}
