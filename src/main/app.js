c75c3b9f61d55f9bfd0e48feed3e5b314adadeb2
Thomas Schweizer <thms.sch@gmail.com>
2019-10-17 21:53:37 -0400

    Fixed exception when closing main window

--- src/main/app.js
+++ src/main/app.js
@@ -93,6 +93,7 @@ function createWindow() {
   // Emitted when the window is closed.
   win.on("closed", () => {
     timer.stop()
+    closeNotifications()
 
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
