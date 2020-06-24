af41351fde33a7bd2977ea61dc304fc511820a2a
Thomas Schweizer <thms.sch@gmail.com>
2020-06-24 17:08:21 -0400

    Auto format

--- src/renderer/about.js
+++ src/renderer/about.js
@@ -10,8 +10,8 @@ configureOpenRenderedLinksInDefaultBrowser()
 function configureOpenRenderedLinksInDefaultBrowser() {
   const aAll = document.querySelectorAll("a")
   if (aAll && aAll.length) {
-    aAll.forEach(function(a) {
-      a.addEventListener("click", function(event) {
+    aAll.forEach(function (a) {
+      a.addEventListener("click", function (event) {
         if (event.target) {
           event.preventDefault()
           let link = event.target.href
