9d1d7215bc04b744203c2ca60340f488b69462dd
Thomas Schweizer <thms.sch@gmail.com>
2023-11-13 10:28:05 -0800

    Add documentation related to MacOS

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,8 @@ app.whenReady().then(() => {
 })
 
 app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') app.quit()
 })
 
